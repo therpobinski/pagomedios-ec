9ca777df49a0535019684c36576a0e49b98fa370
Cristhian Pinto <crispinalejo16@gmail.com>
2021-01-29 14:24:03 -0500

    Pruebas con token-id de transacción

--- test/pagomedios-ec.test.ts
+++ test/pagomedios-ec.test.ts
@@ -4,6 +4,7 @@ import generetePayment, {
   reversePayment,
   Data,
 } from '../src/pagomedios-ec'
+import PagoMediosErrorEc from '../src/pagomedios-ec-error'
 
 function makeBody ({ CIInc = false, bodyInc = false, taxInc = false }) {
   return {
@@ -24,6 +25,12 @@ function makeBody ({ CIInc = false, bodyInc = false, taxInc = false }) {
   }
 }
 
+function errorConnection(e: any) {
+  expect(e).toBeInstanceOf(Error)
+  expect(e.type).toBe(PagoMediosErrorEc.TYPE_CONNECTION)
+  expect(typeof e.message).toEqual('string')
+}
+
 describe('obtener token de pago', () => {
   test('solicitud correcta con datos validados', async () => {
     try {
@@ -32,11 +39,7 @@ describe('obtener token de pago', () => {
       expect(typeof res.message).toEqual('string')
       expect(res.status).toBe(200)
       expect(res.code).toBe(1)
-    } catch (e) {
-      expect(e).toBeInstanceOf(Error)
-      expect(e.type).toBe(PagoMediosErrorEc.TYPE_CONNECTION)
-      expect(typeof e.message).toEqual('string')
-    }
+    } catch (e) { errorConnection(e) }
   })
 
   test('solicitud erronea, con calculos erroneos', async () => {
@@ -73,20 +76,45 @@ describe('obtener token de pago', () => {
   })
 })
 
-// describe('obtener el estado de una transacción', () => {
-//   test('token existente y correcta petición', async () => {
+describe('obtener el estado de una transacción', () => {
+  test('token existente y pago correcto', async () => {
+    try {
+      const tokenId = '2y-13-wszng9xc0u1-swkvxexnaesxbcqin1mvjr55ye2seepjmpbuxmwge'
+      const res = await getStatusLinkPayment(tokenId)
+      expect(typeof res.message).toEqual('string')
+      expect(res.status).toBe(200)
+      expect(res.code).toBe(1)
+    } catch (e) { errorConnection(e) }
+  })
+
+  test('token-id no existe', async () => {
+    try {
+      const res = await getStatusLinkPayment('2y-13-sdfhsbdfkjd-sdfbd')
+    } catch (e) {
+      expect(e).toBeInstanceOf(Error)
+      expect(e.type).toBe(PagoMediosErrorEc.ID_REQUEST)
+      expect(typeof e.message).toEqual('string')
+    }
+  })
+
+  test('estado sin pago', async () => {
+    try {
+      const tokenId = '2y-13--z-jqowebjbq7wj2fvklpufzvpe2nixw1sb28fsqdgt-xr1zeql7u'
+      const res = await getStatusLinkPayment(tokenId)
+    } catch (e) {
+      expect(e).toBeInstanceOf(Error)
+      expect(e.type).toBe(PagoMediosErrorEc.ID_REQUEST)
+      expect(typeof e.message).toEqual('string')
+    }
+  })
+})
+
+// describe('reversar un token pagado', () => {
+//   test('token existente y correcto reverso', async () => {
     
 //   })
 
 //   test('token no existe', async () => {
-    
-//   })
-
-//   test('estado sin pago', async () => {
-    
-//   })
-
-//   test('estado pagado', async () => {
     
 //   })
 // })
